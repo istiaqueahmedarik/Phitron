18e206dbe3d5a1f53af41cd35c28a92e23a971b8
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2023-08-02 23:13:02 +0600

    update

---
+++ week8/MostSociallyDistanced.cpp
@@ -0,0 +1,128 @@
+/*******************************************
+ |I|s|t|i|a|q|u|e| |A|h|m|e|d| |A|r|i|k|
+********************************************/
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+void __print(int x) { cerr << x; }
+#define int long long
+#define pi acos(-1)
+#define endl '\n'
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define FileIO                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout)
+/// decltype and const
+const int MOD = 1e9 + 7, dx[] = {1, 0, -1, 0, 1, -1, -1, 1}, dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
+/// debug
+struct _
+{
+    ios_base::Init i;
+    _()
+    {
+        cin.sync_with_stdio(0);
+        cin.tie(0);
+        cout << fixed << setprecision(10);
+    }
+} ___;
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+/// PBDS
+// template <typename T>
+// using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using o_set_g = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using o_multiset_g = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n + 5);
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+    vector<int> ans;
+    ans.push_back(arr[1]);
+    for (int i = 2; i < n; i++)
+    {
+        vector<int> temp = {arr[i - 1], arr[i], arr[i + 1]};
+        debug(temp);
+        if (!is_sorted(temp.begin(), temp.end()) && !is_sorted(temp.rbegin(), temp.rend()))
+            ans.push_back(arr[i]);
+    }
+    ans.push_back(arr[n]);
+    cout << ans.size() << endl;
+    for (auto i : ans)
+        cout << i << " ";
+    cout << endl;
+}
+
+auto main() -> int32_t
+{
+    IOS;
+    int t, cs = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ week8/zeroReminder.cpp
@@ -0,0 +1,147 @@
+/*******************************************
+ |I|s|t|i|a|q|u|e| |A|h|m|e|d| |A|r|i|k|
+********************************************/
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+void __print(int x) { cerr << x; }
+#define int long long
+#define pi acos(-1)
+#define endl '\n'
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define FileIO                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout)
+/// decltype and const
+const int MOD = 1e9 + 7, dx[] = {1, 0, -1, 0, 1, -1, -1, 1}, dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
+/// debug
+struct _
+{
+    ios_base::Init i;
+    _()
+    {
+        cin.sync_with_stdio(0);
+        cin.tie(0);
+        cout << fixed << setprecision(10);
+    }
+} ___;
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+/// PBDS
+// template <typename T>
+// using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using o_set_g = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T>
+// using o_multiset_g = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    vector<int> rem(n);
+    vector<int> need(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] % k == 0)
+            rem[i] = 0;
+        else
+        {
+            int x = arr[i] / k;
+            int y = x * k;
+            rem[i] = y + k;
+            debug(x, y, i, rem[i]);
+            need[i] = rem[i] - arr[i];
+        }
+    }
+    sort(need.rbegin(), need.rend());
+    unordered_map<int, int> has;
+    for (int i = 0; i < n; i++)
+    {
+        if (need[i] == 0)
+            continue;
+        if (!has[need[i]])
+            has[need[i]]++;
+        else
+        {
+            int past = need[i];
+            need[i] += k * has[need[i]];
+            has[past]++;
+            has[need[i]]++;
+        }
+    }
+    int mx = *max_element(need.begin(), need.end());
+    cout << mx + (mx != 0) << endl;
+}
+
+auto main() -> int32_t
+{
+    IOS;
+    int t, cs = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+//
